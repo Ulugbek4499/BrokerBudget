f0368a494cd704fbcffdff934ad2b44bf3aeb661
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-28 16:04:11 +0500

    save

--- BrokerBudget.Domain/Entities/Purchase.cs
+++ BrokerBudget.Domain/Entities/Purchase.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BrokerBudget.Domain.Common;
 
-namespace BrokerBudget.Domain.Entities.Identity
+namespace BrokerBudget.Domain.Entities
 {
-    internal class Purchase
+    public class Purchase:BaseAuditableEntity
     {
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public decimal Amount { get; set; }
+        public decimal? SaleAmountCategoryPercentage { get; set; }
+
+        public decimal PricePerAmount { get; set; }
+        public decimal? SaleForTotalPrice { get; set; }
+
+        public int? ProductGiverId { get; set; }
+        public virtual ProductGiver? ProductGiver { get; set; }
+
+        public int? ProductTakerId { get; set; }
+        public virtual ProductTaker? ProductTaker { get; set; }
     }
 }
