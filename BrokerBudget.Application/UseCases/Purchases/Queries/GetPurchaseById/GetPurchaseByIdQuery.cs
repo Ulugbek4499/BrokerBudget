c72fe3c668b186078b2fe31e692dde570ca68d4a
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-28 16:51:00 +0500

    save

---
+++ BrokerBudget.Application/UseCases/Purchases/Queries/GetPurchaseById/GetPurchaseByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrokerBudget.Application.UseCases.Purchases.Queries.GetPurchaseById
+{
+    internal class GetPurchaseByIdQuery
+    {
+    }
+}
