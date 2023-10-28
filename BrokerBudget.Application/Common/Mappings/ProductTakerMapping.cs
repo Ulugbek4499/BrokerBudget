71fcb9307802e789266be3e660e9a4f02d611dc8
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-28 16:59:41 +0500

    save

---
+++ BrokerBudget.Application/Common/Mappings/ProductTakerMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BrokerBudget.Application.UseCases.ProductTakers.Commands.CreateProductTaker;
+using BrokerBudget.Application.UseCases.ProductTakers.Commands.DeleteProductTaker;
+using BrokerBudget.Application.UseCases.ProductTakers.Commands.UpdateProductTaker;
+using BrokerBudget.Application.UseCases.ProductTakers;
+using BrokerBudget.Domain.Entities;
+
+namespace BrokerBudget.Application.Common.Mappings
+{
+    public class ProductTakerMapping : Profile
+    {
+        public ProductTakerMapping()
+        {
+            CreateMap<CreateProductTakerCommand, ProductTaker>().ReverseMap();
+            CreateMap<DeleteProductTakerCommand, ProductTaker>().ReverseMap();
+            CreateMap<UpdateProductTakerCommand, ProductTaker>().ReverseMap();
+            CreateMap<ProductTakerResponse, ProductTaker>().ReverseMap();
+
+        }
+    }
+}
