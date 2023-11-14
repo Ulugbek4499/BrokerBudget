7bdf10fa4f10a967dd433c9ef6927e6e408cef16
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-11-14 15:27:24 +0500

    save

--- BrokerBudget.Application/UseCases/ProductTakers/ProductTakerResponse.cs
+++ BrokerBudget.Application/UseCases/ProductTakers/ProductTakerResponse.cs
@@ -4,6 +4,8 @@ namespace BrokerBudget.Application.UseCases.ProductTakers
 {
     public class ProductTakerResponse
     {
+        public int Id { get; set; }
+
         public string CompanyName { get; set; }
         public string ResponsiblePersonName { get; set; }
         public string? PhoneNumber { get; set; }
@@ -12,5 +14,9 @@ namespace BrokerBudget.Application.UseCases.ProductTakers
 
         public virtual ICollection<Purchase>? Purchases { get; set; }
         public virtual ICollection<Payment>? Payments { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public string? ModifiedBy { get; set; }
     }
 }
