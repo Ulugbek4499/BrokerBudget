ee97a082517fbce8d5ba36b75d8f27ac668c96cf
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-11-20 16:26:33 +0500

    save

--- BrokerBudget.Application/UseCases/Reports/StatisticResponse.cs
+++ BrokerBudget.Application/UseCases/Reports/StatisticResponse.cs
@@ -12,42 +12,42 @@ namespace BrokerBudget.Application.UseCases.Reports
         public int CountOfAllProductGivers { get; set; }
         public int CountOfAllProductTakers { get; set; }
 
+
         public decimal AmountOfAllExpenses { get; set; }
         public decimal AmountOfAllExpensesInLastYear { get; set; }
-        public decimal AmountOfAllExpensesInLastSixMonth { get; set; }
-        public decimal AmountOfAllExpensesInLastMonth { get; set; }
+        public decimal AmountOfAllExpensesInCurrentMonth { get; set; }
         public decimal AmountOfAllExpensesInLastWeek { get; set; }
         public decimal AmountOfAllExpensesToday { get; set; }
 
+
+        public Dictionary<string, decimal> AmountOfAllPaymentsByProductTakerName { get; set; }
         public decimal AmountOfAllPaymentsByProductTaker { get; set; }
         public decimal AmountOfAllPaymentsByProductTakerInLastYear { get; set; }
-        public decimal AmountOfAllPaymentsByProductTakerInLastSixMonth { get; set; }
-        public decimal AmountOfAllPaymentsByProductTakerInLastMonth { get; set; }
+        public decimal AmountOfAllPaymentsByProductTakerInCurrentMonth { get; set; }
         public decimal AmountOfAllPaymentsByProductTakerInLastWeek { get; set; }
         public decimal AmountOfAllPaymentsByProductTakerToday { get; set; }
 
+
+        public Dictionary<string, decimal> AmountOfAllPurchasesByProductTakerName { get; set; }
         public decimal AmountOfAllPurchasesByProductTaker { get; set; }
         public decimal AmountOfAllPurchasesByProductTakerInLastYear { get; set; }
-        public decimal AmountOfAllPurchasesByProductTakerInLastSixMonth { get; set; }
-        public decimal AmountOfAllPurchasesByProductTakerInLastMonth { get; set; }
+        public decimal AmountOfAllPurchasesByProductTakerInCurrentMonth { get; set; }
         public decimal AmountOfAllPurchasesByProductTakerInLastWeek { get; set; }
         public decimal AmountOfAllPurchasesByProductTakerToday { get; set; }
 
+
+        public Dictionary<string, decimal> AmountOfAllPaymentsToProductGiverName { get; set; }
         public decimal AmountOfAllPaymentsToProductGiver { get; set; }
         public decimal AmountOfAllPaymentsToProductGiverInLastYear { get; set; }
-        public decimal AmountOfAllPaymentsToProductGiverInLastSixMonth { get; set; }
-        public decimal AmountOfAllPaymentsToProductGiverInLastMonth { get; set; }
+        public decimal AmountOfAllPaymentsToProductGiverInCurrentMonth { get; set; }
         public decimal AmountOfAllPaymentsToProductGiverInLastWeek { get; set; }
         public decimal AmountOfAllPaymentsToProductGiverToday { get; set; }
 
+        public Dictionary<string, decimal> AmountOfAllPurchasesToProductGiverName { get; set; }
         public decimal AmountOfAllPurchasesToProductGiver { get; set; }
         public decimal AmountOfAllPurchasesToProductGiverInLastYear { get; set; }
-        public decimal AmountOfAllPurchasesToProductGiverInLastSixMonth { get; set; }
-        public decimal AmountOfAllPurchasesToProductGiverInLastMonth { get; set; }
+        public decimal AmountOfAllPurchasesToProductGiverInCurrentMonth { get; set; }
         public decimal AmountOfAllPurchasesToProductGiverInLastWeek { get; set; }
         public decimal AmountOfAllPurchasesToProductGiverToday { get; set; }
-
-
-
     }
 }
---
+++ BrokerBudget.MVC/Controllers/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using BrokerBudget.Application.UseCases.Reports;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrokerBudget.MVC.Controllers
+{
+    public class ReportController : ApiBaseController
+    {
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> GetStatistics()
+        {
+            var statistic = await Mediator.Send(new GetStatisticsQuery());
+
+            return View(statistic);
+        }
+    }
+}
